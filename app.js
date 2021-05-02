f0a240489de26a2df18430e78e6bf08e38225c9a
elias-maciel <eliasfsilvamaciel@gmail.com>
2021-05-02 15:54:23 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var submitButton = document.querySelector('#app form button')
+var zipCodeField = document.querySelector('#app form input')
+var content = document.querySelector('#app main')
+
+submitButton.addEventListener('click', run)
+
+function run(event){
+    event.preventDefault()
+
+    var zipCode = zipCodeField.value
+
+    zipCode = zipCode.replace(' ', '')
+    zipCode = zipCode.replace('.', '')
+    zipCode = zipCode.trim()
+
+    axios.
+    get('https://viacep.com.br/ws/' +zipCode + '/json/')
+    .then(function(response){
+        if (response.data.erro) {
+            throw new Error('CEP inválido')
+        }
+
+        content.innerHTML = ''
+        createLine(response.data.logradouro)
+        createLine(response.data.bairro)
+        createLine(response.data.localidade + ' - ' + response.data.uf)
+    })
+    .catch(function(error){
+        content.innerHTML = ''
+        createLine('CEP inválido')
+    })
+
+    console.log(zipCode)
+}
+
+function createLine(text){
+    var line = document.createElement('p')
+    var text = document.createTextNode(text)
+
+    line.appendChild(text)
+    content.appendChild(line)
+}
+
+function mascara_cep(params) {
+    if(zipCodeField.value.length == 5){
+        zipCodeField.value += "-"
+    }
+}
